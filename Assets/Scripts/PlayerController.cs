f7ea0e6cfedbc358405973205455552c9f72afed
tanabee <tanabee1986@gmail.com>
2015-05-31 18:29:33 +0900

    プレイヤーの移動実装

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class PlayerController : MonoBehaviour, IDragHandler {
+
+	public GameObject playerPrefab;
+	public GameObject playerStart;
+	private GameObject player;
+
+	// Use this for initialization
+	void Start () {
+		player = (GameObject)Instantiate (playerPrefab, playerStart.transform.position, Quaternion.identity);
+	}
+
+	public void OnDrag (PointerEventData eventData) {
+		Vector2 moveVector = new Vector2 (eventData.delta.x / 20, 0);
+
+		player.transform.Translate (moveVector);
+	}
+}
