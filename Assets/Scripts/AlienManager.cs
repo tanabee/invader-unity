495bf42f5ec475bd39ceaea086f504b8acbc8179
tanabee <tanabee1986@gmail.com>
2015-05-31 14:11:42 +0900

    敵の一括生成

---
+++ Assets/Scripts/AlienManager.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AlienManager : MonoBehaviour
+{
+
+	public GameObject alienPrefab;
+	private int horizontalLength = 8;
+	private int verticalLength = 5;
+
+	// Use this for initialization
+	void Start ()
+	{
+		// （横は中心、縦は上）を起点として Alien を配置していく
+		for (int x = 1; x <= horizontalLength; x++) {
+			for (int y = 1; y <= verticalLength; y++) {
+				GameObject alien = (GameObject)Instantiate (alienPrefab);
+				Vector2 pos = transform.position;
+				pos.x += alien.transform.localScale.x * (x - ((float)(horizontalLength + 1) / 2)) * 1f;
+				pos.y -= alien.transform.localScale.y * (y - 1) * 1f;
+				Debug.Log (pos);
+				alien.transform.position = pos;
+				alien.transform.SetParent (transform);
+			}
+		}
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
