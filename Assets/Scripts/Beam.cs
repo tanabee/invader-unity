f11e9ffc86ae43a0c53ba48dfe007c2907232a11
tanabee <tanabee1986@gmail.com>
2015-05-31 19:55:52 +0900

    ボタンを押すとビーム発射

---
+++ Assets/Scripts/Beam.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Beam : MonoBehaviour {
+
+	private float speed = 0.2f;
+
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (Vector2.up * speed);
+	}
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,17 +5,24 @@ using UnityEngine.EventSystems;
 public class PlayerController : MonoBehaviour, IDragHandler {
 
 	public GameObject playerPrefab;
-	public GameObject playerStart;
+	public GameObject playerContainer;
 	private GameObject player;
+	public GameObject beamPrefab;
 
 	// Use this for initialization
 	void Start () {
-		player = (GameObject)Instantiate (playerPrefab, playerStart.transform.position, Quaternion.identity);
+		player = (GameObject)Instantiate (playerPrefab, playerContainer.transform.position, Quaternion.identity);
+		player.transform.SetParent (playerContainer.transform);
 	}
 
 	public void OnDrag (PointerEventData eventData) {
-		Vector2 moveVector = new Vector2 (eventData.delta.x / 20, 0);
+		Vector2 moveVector = new Vector2 (eventData.delta.x / 40, 0);
 
-		player.transform.Translate (moveVector);
+		playerContainer.transform.Translate (moveVector);
+	}
+
+	public void ShootBeam() {
+		GameObject beam = (GameObject)Instantiate (beamPrefab, playerContainer.transform.position, Quaternion.identity);
+		Destroy (beam, 3f);
 	}
 }
