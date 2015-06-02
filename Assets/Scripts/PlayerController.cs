6b64c1b873b83208049d235cd92ad1464986294c
tanabee <tanabee1986@gmail.com>
2015-06-03 00:40:49 +0900

    操作性向上

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,13 +3,18 @@ using System.Collections;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class PlayerController : MonoBehaviour, IDragHandler {
+public class PlayerController : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler {
 
 	private GameObject player;
 	public GameObject playerPrefab;
 	public GameObject playerContainer;
 	public GameObject beamPrefab;
 	public Button fireButton;
+	private float speed = 5f;
+
+	private Vector2 beginDragPos;
+	private bool dragging = false;
+	private float dragDirection;
 
 	// Use this for initialization
 	void Start () {
@@ -20,11 +25,32 @@ public class PlayerController : MonoBehaviour, IDragHandler {
 	}
 
 	void Update () {
+
+		if (dragging) {
+			// 画面左下のワールド座標をビューポートから取得
+			Vector2 min = Camera.main.ViewportToWorldPoint (new Vector2 (0.05f, 0));
+			// 画面右上のワールド座標をビューポートから取得
+			Vector2 max = Camera.main.ViewportToWorldPoint (new Vector2 (0.95f, 1));
+
+			Vector2 pos = playerContainer.transform.position;
+			pos.x += dragDirection * speed * Time.deltaTime;
+			pos.x = Mathf.Clamp (pos.x, min.x, max.x);
+
+			playerContainer.transform.position = pos;
+		}
+	}
+
+	public void OnBeginDrag (PointerEventData eventData) {
+		beginDragPos = (Vector2)eventData.worldPosition;
+		dragging = true;
 	}
 
 	public void OnDrag (PointerEventData eventData) {
-		Vector2 moveVector = new Vector2 (eventData.delta.x / 60, 0);
-		playerContainer.transform.Translate (moveVector);
+		dragDirection = ((Vector2)eventData.worldPosition - beginDragPos).x > 0 ? 1f : -1f;
+	}
+
+	public void OnEndDrag (PointerEventData eventData) {
+		dragging = false;
 	}
 
 	public void ShootBeam() {
