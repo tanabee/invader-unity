1ec65c4a61f59ee35bd2df851df14965bde51d3b
tanabee <tanabee1986@gmail.com>
2015-06-06 16:52:02 +0900

    Added LevelLoader

---
+++ Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelLoader : MonoBehaviour {
+	// 画面をロードする
+	public void Load (string sceneName) {
+		Application.LoadLevel (sceneName);
+	}
+}
