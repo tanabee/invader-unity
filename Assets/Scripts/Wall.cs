075e9fb9cbba1a9658c838bcc12f546cda783fc2
tanabee <tanabee1986@gmail.com>
2015-06-08 21:44:02 +0900

    壁の衝突制御

---
+++ Assets/Scripts/Wall.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wall : MonoBehaviour {
+
+	public AudioClip clip;
+
+	void OnTriggerEnter2D(Collider2D collider) {
+		if (collider.gameObject.tag == "PlayerBeam" || collider.gameObject.tag == "AlienBeam") {
+			SoundManager soundManager = GameObject.FindWithTag ("SoundManager").GetComponent<SoundManager> ();
+			soundManager.PlayClip (clip);
+
+			Destroy (collider.gameObject);
+			Destroy (gameObject);
+		}
+	}
+}
