2135bc255ce9de05a9f6b0132e3aba356a41735e
tanabee <tanabee1986@gmail.com>
2015-06-10 23:46:33 +0900

    経過時間表示

---
+++ Assets/Scripts/PlayTimer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayTimer : MonoBehaviour {
+
+	public Text timeLabel;
+	private float time = 0;
+
+	void Update () {
+		time += Time.deltaTime;
+		int minutes = (int)Mathf.Floor(time / 60.0f);
+		int seconds = (int)time % 60;
+		string timeStr = minutes.ToString () + ":" + seconds.ToString ("00");
+		timeLabel.text = timeStr;
+	}
+}
