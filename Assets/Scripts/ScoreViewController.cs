96b745d1fca815ca18106172909c44ebd2dbc1ac
tanabee <tanabee1986@gmail.com>
2015-06-12 22:45:01 +0900

    ハイスコア機能追加

--- Assets/Scripts/ScoreViewController.cs
+++ Assets/Scripts/ScoreViewController.cs
@@ -4,41 +4,54 @@ using System.Collections;
 
 public class ScoreViewController : MonoBehaviour {
 
-	public Text enemyScore;
-	public Text timeScore;
-	public Text totalScore;
+	public Text enemyScoreLabel;
+	public Text timeScoreLabel;
+	public Text totalScoreLabel;
+	public Text highScoreLabel;
 	public Button retryButton;
 
 	// Use this for initialization
 	void Start () {
 
+		// スコア表示
 		ShowScore ();
 
 		// リトライボタンのイベント設定
 		retryButton.onClick.AddListener (OnButtonClick);
 	}
 
+	// スコア表示
 	void ShowScore () {
 
+		// ハイスコア表示
+		int highScore = PlayerPrefs.GetInt ("score");
+		highScoreLabel.text = highScore.ToString();
+
 		// 撃破ポイントを表示
-		enemyScore.text = DataManager.instance.score.ToString();
+		enemyScoreLabel.text = DataManager.instance.score.ToString();
 
 
-		int timeScoreNum = 0;
+		int timeScore = 0;
 		if (DataManager.instance.isCleared) {
 			// クリアタイムのスコアを表示
-			timeScoreNum = (180 - (int)DataManager.instance.playTime) * 10;
-			if (timeScoreNum < 0) {
-				timeScoreNum = 0;
+			timeScore = (180 - (int)DataManager.instance.playTime) * 10;
+			if (timeScore < 0) {
+				timeScore = 0;
 			}
-			timeScore.text = timeScoreNum.ToString ();
+			timeScoreLabel.text = timeScore.ToString ();
 		} else {
-			timeScore.text = "0";
+			timeScoreLabel.text = "0";
 		}
 
 		// トータルのスコアを表示
-		totalScore.text = (DataManager.instance.score + timeScoreNum).ToString();
-		
+		int totalScore = DataManager.instance.score + timeScore;
+		totalScoreLabel.text = (totalScore).ToString();
+
+		// ハイスコアだったら
+		if (totalScore > highScore) {
+			// ローカル保存
+			PlayerPrefs.SetInt ("score", totalScore);
+		}
 	}
 
 	void OnButtonClick () {
