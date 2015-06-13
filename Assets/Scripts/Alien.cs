fb72e53434d8f87936c43ebfec3af260bc596c24
tanabee <tanabee1986@gmail.com>
2015-06-13 15:52:37 +0900

    敵のライフ作成

--- Assets/Scripts/Alien.cs
+++ Assets/Scripts/Alien.cs
@@ -5,15 +5,28 @@ using System.Collections;
 public class Alien : MonoBehaviour {
 
 	public AudioClip clip;
+	public int life;
 
 	void OnTriggerEnter2D(Collider2D collider) {
+
 		if (collider.gameObject.tag == "PlayerBeam") {
+			
+			Destroy (collider.gameObject);
+
+			// ライフを 1 減らす
+			life--;
+
+			// まだ生きていたら return
+			if (life > 0) {
+				return;
+			}
+
+			// 死んだら消す
 			SoundManager soundManager = GameObject.FindWithTag ("SoundManager").GetComponent<SoundManager> ();
 			soundManager.PlayClip (clip);
 
 			DataManager.instance.score += 50;
 
-			Destroy (collider.gameObject);
 			Destroy (gameObject);
 		}
 	}
