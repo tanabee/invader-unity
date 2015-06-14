6f6328ed4279f7dcddaf1a82f2e5ccf38d2bb9fa
tanabee <tanabee1986@gmail.com>
2015-06-14 22:27:47 +0900

    UFO がカメラ外に出たら削除

--- Assets/Scripts/UFO.cs
+++ Assets/Scripts/UFO.cs
@@ -24,6 +24,15 @@ public class UFO : MonoBehaviour {
 		Vector2 pos = transform.position;
 		pos.x -= Time.deltaTime * speed;
 		transform.position = pos;
+
+		// カメラ外に出たら削除
+		// 画面左のワールド座標をビューポートから取得
+		Vector2 min = Camera.main.ViewportToWorldPoint (new Vector2 (-0.1f, 0));
+
+		if (pos.x < min.x) {
+			Destroy (gameObject);
+			soundManager.StopBgClip ();
+		}
 	}
 
 	void OnTriggerEnter2D(Collider2D collider) {
