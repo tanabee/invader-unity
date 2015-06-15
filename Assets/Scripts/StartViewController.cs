b14433ef3cb562a30adb3426bd6afe17ba0e9af8
tanabee <tanabee1986@gmail.com>
2015-06-15 19:36:50 +0900

    ツイートできるように

--- Assets/Scripts/StartViewController.cs
+++ Assets/Scripts/StartViewController.cs
@@ -10,4 +10,9 @@ public class StartViewController : MonoBehaviour {
 	public void TransitionLicenseScene() {
 		Application.LoadLevel ("License");
 	}
+
+	public void Share() {
+		string tweet = "invader game";// TODO: 公開後に URL セット
+		Application.OpenURL("https://twitter.com/intent/tweet?text=" + WWW.EscapeURL(tweet));
+	}
 }
