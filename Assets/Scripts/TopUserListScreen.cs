119993579eb3114e59901303e608e7977bc15f90
tanabee <tanabee1986@gmail.com>
2015-06-17 00:01:10 +0900

    ランキング一覧表示

---
+++ Assets/Scripts/TopUserListScreen.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TopUserListScreen : MonoBehaviour {
+
+	public UserData userData;
+	public GameObject userNodePrefab;
+	public Transform nodeContainer;
+
+
+	IEnumerator Start () {
+		yield return StartCoroutine (userData.Load());
+
+		foreach (UserData.User user in userData.list) {
+
+			GameObject node = (GameObject)Instantiate (userNodePrefab);
+			Transform nodeTf = node.transform;
+			nodeTf.SetParent (nodeContainer, false);
+			nodeTf.Find ("Ranking").GetComponent<Text> ().text = user.ranking.ToString ();
+			nodeTf.Find ("Name").GetComponent<Text> ().text = user.name;
+			nodeTf.Find ("Score").GetComponent<Text> ().text = user.score.ToString ();
+		}
+	}
+}
---
+++ Assets/Scripts/UserData.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using MiniJSON;
+
+public class UserData : MonoBehaviour {
+
+	string url = "http://invader-api.herokuapp.com/v1/ranking";
+
+	[System.Serializable]
+	public class User {
+		public string name;
+		public int score;
+		public int ranking;
+	}
+
+	public List<User> list;
+
+	public IEnumerator Load () {
+		WWW www = new WWW (url);
+
+		using (www) {
+			yield return www;
+
+			if (!string.IsNullOrEmpty (www.error)) {
+				Debug.Log (www.error);
+				yield break;
+			}
+
+			// リスト初期化
+			list = new List<User> ();
+
+			var users = Json.Deserialize (www.text) as List<object>;
+			int ranking = 1;
+			foreach (object obj in users) {
+				var userDict = obj as Dictionary<string, object>;
+
+				var user = new User ();
+				user.name = (string)userDict["name"];
+				user.score = Convert.ToInt32(userDict["score"]);
+				user.ranking = ranking;
+
+				list.Add (user);
+
+				ranking++;
+			}
+			
+		}
+
+	}
+}
