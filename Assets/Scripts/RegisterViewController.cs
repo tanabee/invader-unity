b008e75aa857b933866ab1ffdc6500a98bd89a22
tanabee <tanabee1986@gmail.com>
2015-06-16 15:33:21 +0900

    ユーザー登録画面のたたき作成

---
+++ Assets/Scripts/RegisterViewController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class RegisterViewController : MonoBehaviour {
+
+	public GoogleAnalyticsV3 googleAnalytics;
+	public Text nicknameText;
+
+	void Start () {
+		googleAnalytics.DispatchHits ();
+		googleAnalytics.LogScreen("Register");
+	}
+
+	// スタートボタン押下時
+	public void StartButtonClicked () {
+		Debug.Log (nicknameText.text);
+	}
+}
